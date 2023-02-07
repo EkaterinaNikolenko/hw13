262600176c3fea8213ea1de709069392829b82a9
Nikolenko Front End Pro 24.01.23 <nikolenkoekaterina@gmail.com>
2023-02-07 15:38:20 +0200

    fix

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 function removeElement(arr, item){
-    for(let i = 0; i < arr.length; i++){
-        if(arr[i] == item){
-            arr.splice(arr.indexOf(item), 1);
-        }
-    }
-    return arr;
+    // for(let i = 0; i < arr.length; i++){
+    //     if(arr[i] == item){
+    //         arr.splice(i, 1);
+    //     }
+    // }
+    // return arr;
+    
+    return arr.splice(arr.indexOf(item), 1);;
 }
 
 const array = [1, 2, 3, 4, 5, 6, 7];
